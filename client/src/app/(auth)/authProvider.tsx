2b13159ea8b75cb5ad063c3dcace0a5a19d2fe67
Muhammad Sabeeh Rehman <mure00002@stud.uni-saarland.de>
2025-05-30 18:24:52 +0200

    created authentication

---
+++ client/src/app/(auth)/authProvider.tsx
@@ -0,0 +1,153 @@
+import React, { use } from "react";
+import { Amplify } from "aws-amplify";
+
+import {
+  Authenticator,
+  Heading,
+  Radio,
+  RadioGroupField,
+  useAuthenticator,
+  View,
+} from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import { signIn } from "aws-amplify/auth";
+import { Sign } from "crypto";
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: process.env.NEXT_PUBLIC_AWS_COGNITO_USER_POOL_ID!,
+      userPoolClientId:
+        process.env.NEXT_PUBLIC_AWS_COGNITO_USER_POOL_CLIENT_ID!,
+    },
+  },
+});
+
+const components = {
+  Header() {
+    return (
+      <View className="mt-4 mb-7">
+        <Heading level={3} className="!text-2xl !font-bold">
+          RENT
+          <span className="text-secondary-500 font-light hover:!text-primary-300">
+            IFUL
+          </span>
+        </Heading>
+        <p className="text-muted-foreground mt-2">
+          <span className="font-bold">Welcome!</span> Please sign in to
+          continue.
+        </p>
+      </View>
+    );
+  },
+  SignIn: {
+    Footer() {
+      const { toSignUp } = useAuthenticator();
+      return (
+        <View className="text-center mt-4">
+          <p className="tex=t-muted-foreground">
+            Don&apos;t have an account?{" "}
+            <button
+              onClick={toSignUp}
+              className="text-primary hover:underline bg-transparent border-none p-0"
+            >
+              Sign Up here
+            </button>
+          </p>
+        </View>
+      );
+    },
+  },
+  SignUp: {
+    FormFields() {
+      const { validationErrors } = useAuthenticator();
+      return (
+        <>
+          <Authenticator.SignUp.FormFields />
+          <RadioGroupField
+            legend="Role"
+            name="custom:role"
+            errorMessage={validationErrors?.["custom:role"]}
+            hasError={!!validationErrors?.["custom:role"]}
+            isRequired
+          >
+            <Radio value="tenant" className="!text-primary-500">
+              Tenant
+            </Radio>
+            <Radio value="manager" className="!text-primary-500">
+              Manager
+            </Radio>
+          </RadioGroupField>
+        </>
+      );
+    },
+    Footer() {
+      const { toSignIn } = useAuthenticator();
+      return (
+        <View className="text-center mt-4">
+          <p className="tex=t-muted-foreground">
+            Already have an account?{" "}
+            <button
+              onClick={toSignIn}
+              className="text-primary hover:underline bg-transparent border-none p-0"
+            >
+              Sign in
+            </button>
+          </p>
+        </View>
+      );
+    },
+  },
+};
+const formFields = {
+  signIn: {
+    username: {
+      label: "Email",
+      placeholder: "Email",
+      isRequired: true,
+    },
+    password: {
+      label: "Password",
+      placeholder: "Password",
+      isRequired: true,
+    },
+  },
+  signUp: {
+    username: {
+      order: 1,
+      label: "username",
+      placeholder: "Choose a username",
+      isRequired: true,
+    },
+    email: {
+      order: 2,
+      label: "Email",
+      placeholder: "Enter your email",
+      isRequired: true,
+    },
+    password: {
+      order: 3,
+      label: "Password",
+      placeholder: "Create a password",
+      isRequired: true,
+    },
+    confirm_password: {
+      order: 4,
+      label: "Confirm Password",
+      placeholder: "Confirm your password",
+      isRequired: true,
+    },
+  },
+};
+
+const Auth = ({ children }: { children: React.ReactNode }) => {
+  const { user } = useAuthenticator((context) => [context.user]);
+  return (
+    <div className="h-full">
+      <Authenticator components={components} formFields={formFields}>
+        {() => <>{children}</>}
+      </Authenticator>
+    </div>
+  );
+};
+export default Auth;
