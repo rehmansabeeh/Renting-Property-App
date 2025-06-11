5e02d4627f6a5ef39d500a042028822ae370d0cf
Muhammad Sabeeh Rehman <mure00002@stud.uni-saarland.de>
2025-06-11 14:53:34 +0200

    fixed routing

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,14 @@
 /* eslint-disable */
+import Navbar from "@/components/Navbar";
+import Landing from "./(nondashboard)/landing/page";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="h-full w-full">
+      <Navbar />
+      <main className={`h-full flex w-full flex-col`}>
+        <Landing />
+      </main>
+    </div>
+  );
 }
